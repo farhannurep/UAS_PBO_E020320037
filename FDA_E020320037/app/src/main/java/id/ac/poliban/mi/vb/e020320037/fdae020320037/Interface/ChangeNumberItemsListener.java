58a3afb9c950c57ce768ca4c83bb23038ea60cf5
farhannurep <121595756+farhannurep@users.noreply.github.com>
2023-01-16 14:14:58 +0700

    UAS_PBO_E020320037

---
+++ FDA_E020320037/app/src/main/java/id/ac/poliban/mi/vb/e020320037/fdae020320037/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vb.e020320037.fdae020320037.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ WisataAppE020320037/app/src/main/java/id/ac/poliban/mi/vb/e020320037/wisataappe020320037/decoration/OnClickLayoutMarginItemListener.java
@@ -0,0 +1,10 @@
+package id.ac.poliban.mi.vb.e020320037.wisataappe020320037.decoration;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnClickLayoutMarginItemListener {
+    void onClick(Context context, View v, int position, int spanIndex, RecyclerView.State state);
+}
